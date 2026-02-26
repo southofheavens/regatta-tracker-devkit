273a3b9e14f7dad7d5dce14df62df4705f6a671f
Semen <s.zhuravlev@1440.space>
2026-02-26 18:30:15 +0300

    перенесена функция извлечения JSONа из запроса из микросервиса аутентификации

--- lib/rgt/devkit/General.cpp
+++ lib/rgt/devkit/General.cpp
@@ -1,6 +1,8 @@
 #include <rgt/devkit/General.h>
+#include <rgt/devkit/RGTException.h>
 
 #include <Poco/JSON/Object.h>
+#include <Poco/JSON/Parser.h>
 
 namespace RGT::Devkit
 {
@@ -14,6 +16,27 @@ void sendJsonResponse(Poco::Net::HTTPServerResponse& res,
 
     std::ostream& out = res.send();
     json.stringify(out);
+}
+
+Poco::JSON::Object::Ptr extractJsonObjectFromRequest(Poco::Net::HTTPServerRequest & req)
+{    
+    Poco::JSON::Parser parser;
+
+    Poco::Dynamic::Var result;
+    try {
+        result = parser.parse(req.stream());
+    }
+    catch (...) {
+        throw RGT::Devkit::RGTException("Received invalid json", 
+            Poco::Net::HTTPResponse::HTTPStatus::HTTP_BAD_REQUEST);
+    }
+
+    if (result.type() != typeid(Poco::JSON::Object::Ptr)) {
+        throw RGT::Devkit::RGTException("Expected JSON object, not array", 
+            Poco::Net::HTTPResponse::HTTPStatus::HTTP_BAD_REQUEST);
+    }
+
+    return result.extract<Poco::JSON::Object::Ptr>();
 }
 
 } // namespace RGT::Devkit
--- lib/rgt/devkit/General.h
+++ lib/rgt/devkit/General.h
@@ -6,6 +6,8 @@
 
 #include <Poco/Net/HTTPResponse.h>
 #include <Poco/Net/HTTPServerResponse.h>
+#include <Poco/Net/HTTPServerRequest.h>
+#include <Poco/JSON/Object.h>
 
 namespace RGT::Devkit
 {
@@ -15,6 +17,11 @@ namespace RGT::Devkit
  */
 void sendJsonResponse(Poco::Net::HTTPServerResponse & res,
     const std::string & status, const std::string & message);
+    
+/**
+ * Извлекает из запроса JSON object и возвращает указатель на него
+ */
+Poco::JSON::Object::Ptr extractJsonObjectFromRequest(Poco::Net::HTTPServerRequest & req);
 
 } // namespace RGT::Devkit
 
