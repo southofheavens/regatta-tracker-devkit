d33c79da688af39e0c982c0eccc19fb92ca6044e
Semyon <southofheavens@mail.ru>
2026-03-02 16:18:48 +0300

    функция считывания lua-скрипта перенесена из rgt-auth в devkit

--- lib/rgt/devkit/General.cpp
+++ lib/rgt/devkit/General.cpp
@@ -1,12 +1,23 @@
 #include <rgt/devkit/General.h>
 #include <rgt/devkit/RGTException.h>
 
-#include <Poco/JSON/Object.h>
-#include <Poco/JSON/Parser.h>
+#include <fstream>
 
 namespace RGT::Devkit
 {
 
-
+std::string readLuaScript(const std::string & filename) 
+{
+    std::ifstream file(filename);
+    
+    if (not file.is_open()) {
+        throw std::runtime_error("Cannot open Lua script: " + filename);
+    }
+    return std::string
+    (
+        std::istreambuf_iterator<char>(file),
+        std::istreambuf_iterator<char>()
+    );
+}
 
 } // namespace RGT::Devkit
--- lib/rgt/devkit/General.h
+++ lib/rgt/devkit/General.h
@@ -1,11 +1,15 @@
 #ifndef __GENERAL_H__
 #define __GENERAL_H__
 
+#include <string>
+
 namespace RGT::Devkit
 {
 
-
+// Считывает lua-script из файла с именем filename и возвращает его
+std::string readLuaScript(const std::string & filename);
 
 } // namespace RGT::Devkit
 
 #endif // __GENERAL_H__
+
