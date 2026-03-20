dbb44d8acf9946e24822c8d4b5c615173fbdc174
Semen <s.zhuravlev@1440.space>
2026-03-20 21:37:26 +0300

    добавлена функция для считывания .env файла

--- lib/rgt/devkit/General.cpp
+++ lib/rgt/devkit/General.cpp
@@ -2,6 +2,85 @@
 #include <rgt/devkit/RGTException.h>
 
 #include <fstream>
+#include <format>
+
+namespace
+{
+
+bool checkFormatOfEnvVarName(const std::string & envVarName)
+{
+    if (envVarName.empty()) {
+        return false;
+    }
+
+    if (not std::isupper(envVarName[0])) {
+        return false;
+    }
+
+    auto isLetterValid = [](char letter) -> bool
+    {
+        if (std::isupper(letter) or std::isdigit(letter) or letter == '_') {
+            return true;
+        }
+        return false;
+    };
+
+    for (const char & letter : envVarName)
+    {
+        if (not isLetterValid(letter)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+std::pair<std::string, std::string> parseEnvVar(const std::string & str)
+{
+    size_t equalSignPosition = str.find('=');
+
+    if (equalSignPosition == std::string::npos) {
+        throw std::runtime_error(std::format("Error parsing .env file: Missing equal sign {}", str));
+    }
+
+    std::string envVarName = str.substr(0, equalSignPosition);
+    std::string envVarValue = str.substr(equalSignPosition + 1);
+
+    if (not checkFormatOfEnvVarName(envVarName)) {
+        throw std::runtime_error(std::format("Error parsing .env file: Invalid variable name {}", envVarName));
+    }
+
+    return { std::move(envVarName), std::move(envVarValue) };
+}
+
+void trimString(std::string & string)
+{
+    std::string::const_iterator firstNonSpaceChar = string.cbegin();
+    while (firstNonSpaceChar != string.cend() and std::isspace(*firstNonSpaceChar)) {
+        firstNonSpaceChar++;
+    }
+
+    if (firstNonSpaceChar == string.cend())
+    {
+        string.clear();
+        return;
+    }
+
+    if (firstNonSpaceChar != string.cbegin()) {
+        string.erase(string.cbegin(), firstNonSpaceChar);
+    }
+
+    auto firstNonSpaceCharFromEnd = string.crbegin();
+    while (firstNonSpaceCharFromEnd != string.crend() and std::isspace(*firstNonSpaceCharFromEnd)) {
+        firstNonSpaceCharFromEnd++;
+    }
+
+    if (firstNonSpaceCharFromEnd != string.crbegin()) {
+        string.erase(firstNonSpaceCharFromEnd.base(), string.end());
+    }
+}
+
+} // namespace 
 
 namespace RGT::Devkit
 {
@@ -18,6 +97,40 @@ std::string readLuaScript(const std::string & filename)
         std::istreambuf_iterator<char>(file),
         std::istreambuf_iterator<char>()
     );
+}
+
+void readDotEnv()
+{
+    static bool alreadyRead = false;
+
+    if (alreadyRead) {
+        return;
+    }
+
+    std::fstream file;
+    file.open(".env", std::ios::in);
+    
+    std::string line;
+    while (std::getline(file, line))
+    {
+        trimString(line);
+        std::pair<std::string, std::string> envVar = parseEnvVar(line);
+        if (setenv(envVar.first.c_str(), envVar.second.c_str(), 0) != 0) 
+        {
+            throw std::runtime_error
+            (
+                std::format
+                (
+                    "An error occurred while attempting to set an environment "
+                    "variable with the name {} and the value {}",
+                    envVar.first,
+                    envVar.second
+                )
+            );
+        }
+    }
+
+    alreadyRead = true;
 }
 
 } // namespace RGT::Devkit
--- lib/rgt/devkit/General.h
+++ lib/rgt/devkit/General.h
@@ -9,6 +9,11 @@ namespace RGT::Devkit
 // Считывает lua-script из файла с именем filename и возвращает его
 std::string readLuaScript(const std::string & filename);
 
+/// @brief Считывает и устанавливает переменные окружения, находящиеся в файле .env, который расположен
+/// на одном уровне с meson.build. Если переменная окружения уже задана, перезаписи не будет.
+/// @throw std::runtime_error при ошибке (отсутствует .env, переменные окружения некорректно заданы и т.д.)
+void readDotEnv();
+
 } // namespace RGT::Devkit
 
 #endif // __GENERAL_H__
